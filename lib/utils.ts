0a1849d1e5daf17211f2cf90cb694fd5ebfb4d55
Diogo Oliveira <dluks82@gmail.com>
2024-11-12 01:09:20 -0300

    feat: Create parseLocalDate util

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,11 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export function parseLocalDate(dateString: string): Date {
+  const [year, month, day] = dateString.split('-').map(Number)
+  return new Date(year, month - 1, day, 12, 0, 0)
+}
+
 export function formatCurrency(value: number): string {
   return new Intl.NumberFormat('pt-BR', {
     style: 'currency',
